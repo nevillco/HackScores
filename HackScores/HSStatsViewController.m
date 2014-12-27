92af94aad0116396a90f4c809bf2fbf95462fc0b
nevillco <nevillco@bc.edu>
2014-12-27 15:25:24 -0500

    Organized leaderboard views into IBOutletCollections for cleanliness

--- HackScores/HSStatsViewController.m
+++ HackScores/HSStatsViewController.m
@@ -20,17 +20,10 @@
 @property (weak, nonatomic) IBOutlet HSLeaderBoardView *bestSetsLeaderBoard;
 
 //Each leaderboard box has two HSHackSetView (8 totals)
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestLinesTopHackView;
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestLinesSecondHackView;
-
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestHacksTopHackView;
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestHacksSecondHackView;
-
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestRoundsTopHackView;
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestRoundsSecondHackView;
-
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestSetsTopHackView;
-@property (weak, nonatomic) IBOutlet HSHackSetView *bestSetsSecondHackView;
+@property (strong, nonatomic) IBOutletCollection(HSHackSetView) NSArray* bestLinesHackViews;
+@property (strong, nonatomic) IBOutletCollection(HSHackSetView) NSArray* bestHacksHackViews;
+@property (strong, nonatomic) IBOutletCollection(HSHackSetView) NSArray* bestRoundsHackViews;
+@property (strong, nonatomic) IBOutletCollection(HSHackSetView) NSArray* bestSetsHackViews;
 
 //Button action to play new game (will send user to HSAddPlayersViewController)
 - (IBAction)playNewButtonPressed:(id)sender;
@@ -41,79 +34,114 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self clearContents];
+    
     //Will need delegate object for hackSetData
     AppDelegate* delegate = (AppDelegate*)[[UIApplication sharedApplication]delegate];
-    
-    //Code for "Best Overall Lines" first leaderboard
-    [self.bestLinesLeaderBoard.titleLabel setText:@"Best Overall Lines"];
-    //Sort data (delegate initial sort mode is "Best Overall"
-    [delegate sortHackSetData];
-    //If we have 1 (or 2) HSHackSets, populate 1 (or 2) HSHackSetViews
-    if([delegate getHackSetData].count > 0) {
-        [self.bestLinesTopHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 0]];
-        if([delegate getHackSetData].count > 1)
-            [self.bestLinesSecondHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 1]];
-    }
-    
-    [self.bestHacksLeaderBoard.titleLabel setText:@"Best Single Hacks"];
-    //Change sorting mode
-    [delegate setDataSortMode: SortByBestHack];
-    //Sort data (delegate initial sort mode is "Best Overall"
-    [delegate sortHackSetData];
-    //If we have 1 (or 2) HSHackSets, populate 1 (or 2) HSHackSetViews
-    if([delegate getHackSetData].count > 0) {
-        [self.bestHacksTopHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 0]];
-        if([delegate getHackSetData].count > 1)
-            [self.bestHacksSecondHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 1]];
-    }
-
-    [self.bestRoundsLeaderBoard.titleLabel setText:@"Best Rounds (30 Hacks)"];
-    //Change sorting mode
-    [delegate setDataSortMode: SortByBestRound];
-    //Sort data (delegate initial sort mode is "Best Overall"
-    [delegate sortHackSetData];
-    //If we have 1 (or 2) HSHackSets, populate 1 (or 2) HSHackSetViews
-    if([delegate getHackSetData].count > 0) {
-        [self.bestRoundsTopHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 0]];
-        if([delegate getHackSetData].count > 1)
-            [self.bestRoundsSecondHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 1]];
-    }
-
-    [self.bestSetsLeaderBoard.titleLabel setText:@"Best Sets (3 Rounds)"];
-    //Change sorting mode
-    [delegate setDataSortMode: SortBySetScore];
-    //Sort data (delegate initial sort mode is "Best Overall"
-    [delegate sortHackSetData];
-    //If we have 1 (or 2) HSHackSets, populate 1 (or 2) HSHackSetViews
-    if([delegate getHackSetData].count > 0) {
-        [self.bestSetsTopHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 0]];
-        if([delegate getHackSetData].count > 1)
-            [self.bestSetsSecondHackView addHackContent:[[delegate getHackSetData] objectAtIndex: 1]];
-    }
-
+    [self populateBestLinesLeaderboard:delegate];
+    [self populateBestHacksLeaderboard:delegate];
+    [self populateBestRoundsLeaderboard:delegate];
+    [self populateBestSetsLeaderboard:delegate];
 }
 
-- (void) clearContents {
-    [[self.bestLinesTopHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    [[self.bestLinesSecondHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
+- (void) populateBestLinesLeaderboard: (AppDelegate*) delegate {
+    //Title text
+    [self.bestLinesLeaderBoard.titleLabel setText:@"Best Overall Lines"];
     
-    [[self.bestHacksTopHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    [[self.bestHacksSecondHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    //Sort by appropriate mode
+    [delegate setDataSortMode:SortByBestLine];
+    [delegate sortHackSetData];
     
-    [[self.bestRoundsTopHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    [[self.bestRoundsSecondHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    //Populate as many views as possible (bounded by how many views
+    //are on screen and by how many data objects we have)
+    int numViewsToPopulate = (int)MIN([delegate getHackSetData].count,
+                                 self.bestLinesHackViews.count);
     
-    [[self.bestSetsTopHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    [[self.bestSetsSecondHackView subviews]
-     makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    for(int i = 0; i < numViewsToPopulate; i++) {
+        //Remove all content from HSHackSetView
+        //in case view was previously loaded
+        [[[self.bestLinesHackViews objectAtIndex:i] subviews]
+         makeObjectsPerformSelector:@selector(removeFromSuperview)];
+        
+        //Repopulate with current HSHackSet object
+        [[self.bestLinesHackViews objectAtIndex:i] addHackContent:
+         [[delegate getHackSetData] objectAtIndex: i]];
+    }
+}
+
+- (void) populateBestHacksLeaderboard: (AppDelegate*) delegate {
+    //Title text
+    [self.bestHacksLeaderBoard.titleLabel setText:@"Best Single Hacks"];
+    
+    //Sort by appropriate mode
+    [delegate setDataSortMode:SortByBestHack];
+    [delegate sortHackSetData];
+    
+    //Populate as many views as possible (bounded by how many views
+    //are on screen and by how many data objects we have)
+    int numViewsToPopulate = (int)MIN([delegate getHackSetData].count,
+                                      self.bestHacksHackViews.count);
+    
+    for(int i = 0; i < numViewsToPopulate; i++) {
+        //Remove all content from HSHackSetView
+        //in case view was previously loaded
+        [[[self.bestHacksHackViews objectAtIndex:i] subviews]
+         makeObjectsPerformSelector:@selector(removeFromSuperview)];
+        
+        //Repopulate with current HSHackSet object
+        [[self.bestHacksHackViews objectAtIndex:i] addHackContent:
+         [[delegate getHackSetData] objectAtIndex: i]];
+    }
+}
+
+- (void) populateBestRoundsLeaderboard: (AppDelegate*) delegate {
+    //Title text
+    [self.bestRoundsLeaderBoard.titleLabel setText:
+     [NSString stringWithFormat: @"Best Rounds (%d hacks)", [AppDelegate HACKS_PER_ROUND]]];
+    
+    //Sort by appropriate mode
+    [delegate setDataSortMode:SortByBestRound];
+    [delegate sortHackSetData];
+    
+    //Populate as many views as possible (bounded by how many views
+    //are on screen and by how many data objects we have)
+    int numViewsToPopulate = (int)MIN([delegate getHackSetData].count,
+                                      self.bestRoundsHackViews.count);
+    
+    for(int i = 0; i < numViewsToPopulate; i++) {
+        //Remove all content from HSHackSetView
+        //in case view was previously loaded
+        [[[self.bestRoundsHackViews objectAtIndex:i] subviews]
+         makeObjectsPerformSelector:@selector(removeFromSuperview)];
+        
+        //Repopulate with current HSHackSet object
+        [[self.bestRoundsHackViews objectAtIndex:i] addHackContent:
+         [[delegate getHackSetData] objectAtIndex: i]];
+    }
+}
+
+- (void) populateBestSetsLeaderboard: (AppDelegate*) delegate {
+    //Title text
+    [self.bestSetsLeaderBoard.titleLabel setText: @"Best Sets (3 Rounds)"];
+    
+    //Sort by appropriate mode
+    [delegate setDataSortMode:SortBySetScore];
+    [delegate sortHackSetData];
+    
+    //Populate as many views as possible (bounded by how many views
+    //are on screen and by how many data objects we have)
+    int numViewsToPopulate = (int)MIN([delegate getHackSetData].count,
+                                      self.bestSetsHackViews.count);
+    
+    for(int i = 0; i < numViewsToPopulate; i++) {
+        //Remove all content from HSHackSetView
+        //in case view was previously loaded
+        [[[self.bestSetsHackViews objectAtIndex:i] subviews]
+         makeObjectsPerformSelector:@selector(removeFromSuperview)];
+        
+        //Repopulate with current HSHackSet object
+        [[self.bestSetsHackViews objectAtIndex:i] addHackContent:
+         [[delegate getHackSetData] objectAtIndex: i]];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
